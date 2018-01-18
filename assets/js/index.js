02e6bb9b1c7be0549015fb15cf7b2ee63da3c120
carolina.silvag <carolinasguzman@gmail.com>
2018-01-18 19:57:27 -0300

    algunas cosas

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@
   $.fn.extend({
     cardify: function() {
       return this.each(function() {
-        $(this).find('img').css({'width': '100%'});
+        $(this).find('img').css({'width': '100%', 'height': '350px'});
         $(this).find('img').wrap('<figure></figure>');
         $(this).find('img').each(function(index, el) {
           var text = $(el).attr('alt');
@@ -14,9 +14,12 @@
         $('.text').hide();
         $(this).find('figure').mouseover(function() {
           $(this).find('.text').show();
+          $(this).find('.text').css({'position': 'absolute','top': '50%','left': '50%','transform': 'translateX(-50%) translateY(-50%)','margin': '0','text-align': 'center'});
+          $(this).parent().css({'position': 'relative','z-index': '1','-webkit-transform':'scale(1.2)','-moz-transform':'scale(1.2)','-ms-transform':'scale(1.2)','-o-transform':'scale(1.2)','transform':'scale(1.2)'})
         });
         $(this).find('figure').mouseleave(function(event) {
           $(this).find('.text').hide();
+          $(this).parent().css({'position': 'relative','z-index': '0','-webkit-transition':'scale(1.0)','-moz-transform':'scale(1.0)','-ms-transform':'scale(1.0)','-o-transform':'scale(1.0)','transform':'scale(1.0)'});
         });
       });
     }
